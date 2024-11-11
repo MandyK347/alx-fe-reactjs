046aed45d3e631943cb618a84b138cccc1059c7f
MandyK347 <mandy.nnk@gmail.com>
2024-11-11 12:48:30 +0200

    Initial Commit

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,12 @@
+// src/recipeStore.js
+import create from 'zustand'
+
+const useRecipeStore = create(set => ({
+    recipes: [], // Initial empty array for recipes
+    addRecipes: (newRecipe) => set(state => ({
+        recipes: [...state.recipes, newREcipe] // Add new recipe to the list
+    })),
+    setRecipes: (recipes) => set({ recipes }) // Set a new list of recipes
+}));
+
+export default recipeStore;
