c2a487aebab0665b100cc4836b21df9f4b40ecbf
MandyK347 <mandy.nnk@gmail.com>
2024-11-11 12:55:57 +0200

    Initial Commit

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,12 @@
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1> Hello everyone, I am learning React at ALX!</h1>
+            <P>This is a simple JSX component.</P>
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+}
+
+
+export default WelcomeMessage;
