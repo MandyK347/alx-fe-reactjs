132bd22691d932ae2a14ef051f762a43df8d86dd
MandyK347 <mandy.nnk@gmail.com>
2024-11-13 19:31:01 +0200

    Updated RecipeList.jsx file

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,6 +1,7 @@
 // src/components/RecipeList.jsx
 import React, { useEffect } from 'react';
 import { useRecipeStore } from '../recipeStore';
+import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);
@@ -21,7 +22,9 @@ const RecipeList = () => {
             ) : (
               displayRecipes.map(recipe => (
                 <div key={recipe.id}>
-                    <h3>{recipe.title}</h3>
+                    <h3>{recipe.title}
+                       <Link to={'/recipe/${recipe.id}'}>{recipe.title}</Link>
+                    </h3>
                     <p>{recipe.description}</p>
                 </div>
               ))
