9fc0a83dc8db7b9f74d9c9d05460ad71a9b69956
MandyK347 <mandy.nnk@gmail.com>
2024-11-13 13:46:29 +0200

    Updated DeleteRecipeButton.jsx file

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,10 +1,13 @@
 import { useRecipeStore } from '../recipeStore';
+import { useNavigate } from 'react-router-dom'; // Add this import
 
 const DeleteRecipeButton = ({ recipeId }) => {
     const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
+    const navigate = useNavigate() //Use useNavigate
 
     const handleDelete = () => {
         deleteRecipe(recipeId);
+        nagivate('/'); //Redirect to recipe list after deletion
     };
 
     return (
