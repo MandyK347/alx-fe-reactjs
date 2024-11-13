1d90f503f19c25d0df560b45773004ac487ace2a
MandyK347 <mandy.nnk@gmail.com>
2024-11-13 10:17:06 +0200

    Updated files

--- recipe-sharing-app/src/components/RecipeDetails.js
+++ recipe-sharing-app/src/components/RecipeDetails.js
@@ -1,29 +1,25 @@
 import { useRecipeStore } from '../recipeStore'
-import { useHistory } from '.react-router-dom';
+import { useParams } from 'react-router-dom';
 import EditRecipeForm from './EditRecipeForm';
 import DeleteRecipeButton from './DeleteRecipeButton';
 
-const RecipeDetails = ({ recipeId }) => {
-    const recipe = useRecipeStore(state =>
-        state.recipe.find(recipe => recipe.id === recipeId)
+const RecipeDetails = () => {
+    const { recipeId } = useParams();
+    const recipe = useRecipeStore((state) =>
+        state.recipe.find((recipe) => recipe.id === parseInt(recipeId))
     );
-
-    const history = useHistory();
-
+    
+    // If recipe doesn't exist, show a "not found" message
     if (!recipe) {
-        return <p>Recipe not found.</p>;
+        return <p>Recipe not found!</p>;
     }
 
     return (
         <div>
             <h1>{recipe.title}</h1>
             <p>{recipe.description}</p>
-            <div>
-              {/* Pass recipe ID to EditRecipeForm */}
-              <EditRecipeForm recipe={recipe} />
-              {/* Pass recipe ID to DeleteRecipeButton */}
-              <DeleteRecipeButton recipeId={recipe.id} />
-           </div>
+            <EditRecipeForm recipe={recipe} />
+            <DeleteRecipeButton recipeId={recipe.id} />
         </div>
     );
 };
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,12 +4,16 @@ import create from 'zustand'
 const useRecipeStore = create((set) => ({
     recipe:[],
     addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
-    updateRecipe: (id, updatedRecipe) =>
-        set((state) => ({
-            recipe: state.recipes.map((recipe) => (recipe.id === id ? updatedRecipe : recipe)),
-        })),
-    deleteRecipe: (id) =>
-        set((state) => ({recipes: state.recipes.filter((recipe) => recipe.id !== id) })),
+    
+    deleteRecipe: (id) => set((state) => ({
+        recipe: state.recipe.filter((recipe) => recipe.id !== id)
+    })),
+
+    updateRecipe: (id, updatedRecipe) => set((state) => ({
+        recipe: state.recipe.map((recipe) =>
+            recipe.id === id ? updatedRecipe : recipe
+        ),
+    })),
 }));
 
 
