d6d420078da3213a110b1ae246869fb0c95adbf5
MandyK347 <mandy.nnk@gmail.com>
2024-11-21 08:45:18 +0200

    Updated BlogPost.jsx

---
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -0,0 +1,11 @@
+// src/components/BlogPost.jsx
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const Post = () => {
+    const { id } = useParams(); //Retrives the dynamic segment from the URL
+
+    return <h2>Blog Post: {id}</h2>;
+};
+
+export default BlogPost;
