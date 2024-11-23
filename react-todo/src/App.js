c43c6e61e788f5457e650c26e29af7d4ef89ae66
MandyK347 <mandy.nnk@gmail.com>
2024-11-23 11:58:31 +0200

    Updated App.js

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
+import TodoList from 'src/components/TodoList';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Buy groceries', completed: false },
+    { id: 2, text: 'Walk the dog', completed: false },
+    { id: 3, text: 'Clean the house', completed: false },
+  ]);
+
+  // Function to toggle completion status
+  const toggleTodo = (id) => {
+    setTodos(prevTodos =>
+      prevTodos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  
   return (
     <div className="App">
-      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +35,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1>Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
     </div>
   );
 }
