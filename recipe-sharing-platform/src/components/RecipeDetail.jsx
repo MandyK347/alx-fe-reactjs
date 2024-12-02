98e0cdf36244fa580a779c050b3aeba3254e74f5
MandyK347 <mandy.nnk@gmail.com>
2024-12-02 13:04:14 +0200

    Updated RecipeDetails.jsx file

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 function RecipeDetail() {
@@ -20,15 +20,19 @@ function RecipeDetail() {
 
     return (
         <div className="container mx-auto p-4">
-            <div className="max-w-2xl mx-auto">
-                <img src={recipe.image} alt={recipe.name} className="w-full h-auto rounded-lg mb-4" />
+            <div className="max-w-2xl mx-auto bg-white p-6 rounded-lg shadow-lg">
+                {/* Recipe Image with shadow */}
+                <img src={recipe.image} alt={recipe.name} className="w-full h-auto rounded-lg mb-4 shadow-md" />
+                {/* Recipe Title */}
                 <h1 className="text-3xl font-bold mb-4">{recipe.name}</h1>
+                {/* Ingredients Section */}
                 <h2 className="text-xl font-semibold mb-2">Ingredients</h2>
                 <ul className="list-dics pl-5 mb-6">
                     {recipe.ingredients.map((ingredient, index) => (
                         <li key={index} className="text-lg">{ingredient}</li>
                     ))}
                 </ul>
+                {/* Cooking Instruction Section */}
                 <h2 className="text-xl font-semibold mb-2">Cooking Instrations</h2>
                 <ol className="list-decimal pl-5 space-y-2">
                     {recipe.instructions.map((step, index) => (
